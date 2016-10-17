74f7118badec54234912372faacc4566d8a4f96c
Chris Cadek <purzelification@gmail.com>
2016-10-17 21:05:28 +0200

    Angefangen mit Integration der Shell am Client

--- src/main/java/client/Client.java
+++ src/main/java/client/Client.java
@@ -92,6 +92,8 @@ public class Client implements IClientCli, Runnable {
 	public static void main(String[] args) {
 		Client client = new Client(args[0], new Config("client"), System.in,
 				System.out);
+		IClientCli clientShell = new ClientCli(new Config("client"),"client-shell",System.in,System.out);
+		new Thread((Runnable) clientShell).start();
 		//ExecutorService e = Executors.newCachedThreadPool();
 		//e.execute(client);
 		//(new Thread(client)).start();
