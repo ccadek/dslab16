623e5b5ef09157aecc7ed0c91ca44d92d83c75b5
Chris Cadek <purzelification@gmail.com>
2016-10-26 01:35:12 +0200

    Adds ClientFactory

---
+++ src/main/java/client/ClientFactory.java
@@ -0,0 +1,30 @@
+package client;
+
+
+import util.Config;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+/*
+    Simple class for creating instances of commonly used classes
+ */
+public class ClientFactory {
+
+    private static Config config = new Config("client");
+
+    public static Socket createSocket() throws IOException{
+        return new Socket(config.getString("chatserver.host"),config.getInt("chatserver.tcp.port"));
+    }
+
+    public static BufferedReader createBufferedReader(Socket socket) throws IOException {
+        return new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    }
+
+    public static PrintWriter createPrintWriter(Socket socket) throws IOException {
+        return new PrintWriter(socket.getOutputStream(),true);
+    }
+}
