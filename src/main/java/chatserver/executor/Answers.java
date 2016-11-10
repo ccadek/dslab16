81fdb0bbf98acd749da5cbece35f24d1ce83fda1
Chris Cadek <purzelification@gmail.com>
2016-11-10 20:29:23 +0100

    Answers: All Strings are final, and a new message for the user already being logged in

--- src/main/java/chatserver/executor/Answers.java
+++ src/main/java/chatserver/executor/Answers.java
@@ -5,13 +5,15 @@ public class Answers {
 
 	public static final String INVALID_REQUEST = "This is not a valid request.";
 
-	public static String NOT_LOGGED_IN = "Not logged in.";
+	public static final String NOT_LOGGED_IN = "Not logged in.";
 
-	public static String SUCCESS_LOGIN = "Successfully logged in.";
+	public static final String SUCCESS_LOGIN = "Successfully logged in.";
 
-	public static String INVALID_LOGIN = "Wrong username and/or password.";
+	public static final String ALREADY_LOGGED_IN = "Already logged in.";
 
-	public static String SUCCESS_LOGOUT = "Successfully logged out.";
+	public static final String INVALID_LOGIN = "Wrong username and/or password.";
 
-	public static String SUCCESS_REGISTER = "Successfully registered address for";
+	public static final String SUCCESS_LOGOUT = "Successfully logged out.";
+
+	public static final String SUCCESS_REGISTER = "Successfully registered address for";
 }
